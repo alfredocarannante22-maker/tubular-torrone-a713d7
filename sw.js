06dbbe0881dbe8d28e596fc9f6eaa9c91e3c168d
Alfredo Carannante <alfredocarannante22@gmail.com>
2026-05-15 13:08:11 +0200

    SW v3: network-first per aggiornamenti immediati

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'famiglia-v2';
+const CACHE = 'famiglia-v3';
 const ASSETS = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', e => {
@@ -17,6 +17,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
