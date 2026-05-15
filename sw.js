91e16432db3d6a4455be71d37bc1c28adfb89fb2
Alfredo Carannante <alfredocarannante22@gmail.com>
2026-05-15 13:12:00 +0200

    SW: ripristina cache-first, fix fetch GET only

--- sw.js
+++ sw.js
@@ -16,7 +16,8 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
